b262c24bf45a0985c13935a0563f1939f9043b57
fmotion1 <futuremotionrecovery@gmail.com>
2023-09-30 04:29:14 -0500

    Updates to Module Manifests

---
+++ VSYS-Dev-DropMITLicense.ps1
@@ -0,0 +1,11 @@
+﻿param (
+    [Parameter(Mandatory,ValueFromPipeline)]
+    $Folder,
+
+    [Parameter(Mandatory=$false)]
+    [Switch]
+    $Force
+)
+
+Save-LicenseToFolder -Folder $Folder -LicenseType MIT -Force:$Force
+
